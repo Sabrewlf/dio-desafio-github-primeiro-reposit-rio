2edf68fecdf2eb18af09de9c3e4c2a084e8f74a7
Marcos Antonio Garcia <marcos464_20@hotmail.com>
2022-06-14 17:57:49 -0300

    Finalizando projeto Contador

---
+++ Módulo II - Html-Css-Js/Curso Java Script/Exercícios/js/scripts.js
@@ -0,0 +1,14 @@
+var currenteNumberWrapper = document.getElementById('currentNumber');
+var currenteNumber = 0;
+
+function increment()
+{
+    currenteNumber = currenteNumber + 1;
+    currenteNumberWrapper.innerHTML = currenteNumber;
+}
+
+function decrement()
+{
+    currenteNumber = currenteNumber - 1;
+    currenteNumberWrapper.innerHTML = currenteNumber;
+}
